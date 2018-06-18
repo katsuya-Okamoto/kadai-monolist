52425296149ba17774b6c3a3f308a092e8b52c9a
katsuya.Okamoto <sume425@eay.jp>
2018-06-18 09:29:25 +0000

    want have

--- app/User.php
+++ app/User.php
@@ -26,4 +26,102 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    public function items()
+    {
+        return $this->belongsToMany(Item::class)->withPivot('type')->withTimestamps();
+    }
+    
+    public function want_items()
+    {
+        return $this->items()->where('type', 'want');
+    }
+
+    public function have_items()
+    {
+        return $this->items()->where('type', 'have');
+    }
+
+    public function want($itemId)
+    {
+        //既にwantしているかの確認
+        $exist = $this->is_wanting($itemId);
+        
+        if ($exist){
+            //既にwantしていれば何もしない
+            return false;
+        }else{
+            //未wantであればwantする
+            $this->items()->attach($itemId, ['type' => 'want']);
+            return true;
+        }
+    }
+    
+    public function dont_want($itemId)
+    {
+        //既にwantしているかの確認
+        $exist = $this->is_wanting($itemId);
+        
+        if ($exist){
+            //既にwantしていればWantを外す
+            \DB::delete("DELETE FROM item_user WHERE user_id = ? AND item_id = ? AND type='want'", [\Auth::user()->id, $itemId]);
+            return true;
+        }else{
+            //未wantであればなにもしない
+            return false;
+        }
+    }
+    
+    public function is_wanting($itemIdOrCode)
+    {
+        if (is_numeric($itemIdOrCode)){
+            $item_id_exists = $this->want_items()->where('item_id', $itemIdOrCode)->exists();
+            return $item_id_exists;
+        }else{
+            $item_code_exists = $this->want_items()->where('Code', $itemIdOrCode)->exists();
+            return $item_code_exists;
+        }
+    }
+
+    public function have($itemId)
+    {
+        //既にwantしているかの確認
+        $exist = $this->is_having($itemId);
+        
+        if ($exist){
+            //既にhaveしていれば何もしない
+            return false;
+        }else{
+            //未haveであればhaveする
+            $this->items()->attach($itemId, ['type' => 'have']);
+            return true;
+        }
+    }
+    
+    public function dont_have($itemId)
+    {
+        //既にwantしているかの確認
+        $exist = $this->is_having($itemId);
+        
+        if ($exist){
+            //既にhaveしていればhaveを外す
+            \DB::delete("DELETE FROM item_user WHERE user_id = ? AND item_id = ? AND type='have'", [\Auth::user()->id, $itemId]);
+            return true;
+        }else{
+            //未haveであればなにもしない
+            return false;
+        }
+    }
+    
+    public function is_having($itemIdOrCode)
+    {
+        if (is_numeric($itemIdOrCode)){
+            $item_id_exists = $this->have_items()->where('item_id', $itemIdOrCode)->exists();
+            return $item_id_exists;
+        }else{
+            $item_code_exists = $this->have_items()->where('Code', $itemIdOrCode)->exists();
+            return $item_code_exists;
+        }
+    }
+    
 }
